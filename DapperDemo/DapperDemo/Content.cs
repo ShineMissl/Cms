54769506b72b164f8135e6c3b9aa98ef81569838
ShineMissl <zhao888chuanjia@163.com>
2019-12-19 09:13:29 +0800

    Init Dapper Demo

---
+++ DapperDemo/DapperDemo/Content.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DapperDemo
+{
+    public class Content
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string title { get; set; }
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 状态 1正常 0删除
+        /// </summary>
+        public int status { get; set; }
+        /// <summary>
+        /// 创建时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+        /// <summary>
+        /// 修改时间
+        /// </summary>
+        public DateTime? modify_time { get; set; }
+    }
+    public class Comment
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int id { get; set; }
+        /// <summary>
+        /// 文章id
+        /// </summary>
+        public int content_id { get; set; }
+        /// <summary>
+        /// 评论内容
+        /// </summary>
+        public string content { get; set; }
+        /// <summary>
+        /// 添加时间
+        /// </summary>
+        public DateTime add_time { get; set; } = DateTime.Now;
+    }
+}
---
+++ DapperDemo/DapperDemo/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            insertSingleData();
+            insertMutilData();
+            Console.ReadLine();
+        }
+
+        static void insertSingleData()
+        {
+            var content = new Content()
+            {
+                title = "Title1",
+                content = "Content1"
+            };
+            using (var conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Practice\\Cms\\DapperDemo\\DapperDemo\\Database1.mdf;Integrated Security=True"))
+            {
+                string sql_insert = @"Insert into [Content] 
+                    values (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_insert：插入了{result}条数据！");
+
+            }
+        }
+
+        /// <summary>
+        /// 测试一次批量插入两条数据
+        /// </summary>
+        static void insertMutilData()
+        {
+            List<Content> contents = new List<Content>();
+            contents.Add(new Content
+            {
+                title = "批量插入标题1",
+                content = "批量插入内容1",
+
+            });
+            contents.Add(new Content
+            {
+                title = "批量插入标题2",
+                content = "批量插入内容2",
+
+            });
+
+            using (var conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Practice\\Cms\\DapperDemo\\DapperDemo\\Database1.mdf;Integrated Security=True"))
+            {
+                string sql_insert = @"INSERT INTO [Content]
+                    (title, [content], status, add_time, modify_time)
+                    VALUES   (@title,@content,@status,@add_time,@modify_time)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_insert：插入了{result}条数据！");
+            }
+        }
+    }
+}
