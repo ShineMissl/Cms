48e442206c56d4bbbcc5a017b0fd1191f7cd5412
ShineMissl <zhao888chuanjia@163.com>
2019-12-19 10:14:09 +0800

    Add delete update method

--- DapperDemo/DapperDemo/Program.cs
+++ DapperDemo/DapperDemo/Program.cs
@@ -7,11 +7,18 @@ namespace DapperDemo
 {
     class Program
     {
+        private static string connectString =
+                "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Practice\\Cms\\DapperDemo\\DapperDemo\\Database1.mdf;Integrated Security=True"
+            ;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            insertSingleData();
-            insertMutilData();
+            //insertSingleData();
+            //insertMutilData();
+            //test_del();
+            //test_mult_del();
+            test_update();
+            test_mult_update();
             Console.ReadLine();
         }
 
@@ -22,9 +29,9 @@ namespace DapperDemo
                 title = "Title1",
                 content = "Content1"
             };
-            using (var conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Practice\\Cms\\DapperDemo\\DapperDemo\\Database1.mdf;Integrated Security=True"))
+            using (var conn = new SqlConnection(connectString))
             {
-                string sql_insert = @"Insert into [Content] 
+                string sql_insert = @"Insert into Content
                     values (@title,@content,@status,@add_time,@modify_time)";
                 var result = conn.Execute(sql_insert, content);
                 Console.WriteLine($"test_insert：插入了{result}条数据！");
@@ -51,13 +58,108 @@ namespace DapperDemo
 
             });
 
-            using (var conn = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=E:\\Practice\\Cms\\DapperDemo\\DapperDemo\\Database1.mdf;Integrated Security=True"))
+            using (var conn = new SqlConnection(connectString))
             {
                 string sql_insert = @"INSERT INTO [Content]
                     (title, [content], status, add_time, modify_time)
                     VALUES   (@title,@content,@status,@add_time,@modify_time)";
                 var result = conn.Execute(sql_insert, contents);
                 Console.WriteLine($"test_mult_insert：插入了{result}条数据！");
+            }
+        }
+
+        static void test_del()
+        {
+            var content = new Content
+            {
+                id = 2,
+
+            };
+            using (var conn = new SqlConnection(connectString))
+            {
+                string sql_insert = @"DELETE FROM [Content] WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_del：删除了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 测试一次批量删除两条数据
+        /// </summary>
+        static void test_mult_del()
+        {
+            List<Content> contents = new List<Content>() {
+                new Content
+                {
+                    id=3,
+
+                },
+                new Content
+                {
+                    id=4,
+
+                },
+            };
+
+            using (var conn = new SqlConnection(connectString))
+            {
+                string sql_insert = @"DELETE FROM [Content] WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_del：删除了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 测试修改单条数据
+        /// </summary>
+        static void test_update()
+        {
+            var content = new Content
+            {
+                id = 5,
+                title = "标题5",
+                content = "内容5",
+
+            };
+            using (var conn = new SqlConnection(connectString))
+            {
+                string sql_insert = @"UPDATE  [Content]
+                    SET         title = @title, [content] = @content, modify_time = GETDATE()
+                    WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, content);
+                Console.WriteLine($"test_update：修改了{result}条数据！");
+            }
+        }
+
+        /// <summary>
+        /// 测试一次批量修改多条数据
+        /// </summary>
+        static void test_mult_update()
+        {
+            List<Content> contents = new List<Content>() {
+                new Content
+                {
+                    id=6,
+                    title = "批量修改标题6",
+                    content = "批量修改内容6",
+
+                },
+                new Content
+                {
+                    id =7,
+                    title = "批量修改标题7",
+                    content = "批量修改内容7",
+
+                },
+            };
+
+            using (var conn = new SqlConnection(connectString))
+            {
+                string sql_insert = @"UPDATE  [Content]
+                    SET         title = @title, [content] = @content, modify_time = GETDATE()
+                    WHERE   (id = @id)";
+                var result = conn.Execute(sql_insert, contents);
+                Console.WriteLine($"test_mult_update：修改了{result}条数据！");
             }
         }
     }
